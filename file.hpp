7d103950d63dff5d3c794c3330ecf74c05f460f3
Polaroot <130406743+Polaroot@users.noreply.github.com>
2023-08-10 19:10:22 +0200

    Update file.hpp

--- file.hpp
+++ file.hpp
@@ -1,7 +1,7 @@
 #pragma once
+
 #include <string>
 #include <Windows.h>
-
 
 namespace win32
 {
@@ -9,54 +9,35 @@ namespace win32
     {
     private:
 
-        struct
-        {
-            std::string fileName = nullptr;
-
-            HANDLE fileHandle = INVALID_HANDLE_VALUE;
-        };
+        std::string fileName = nullptr;
 
     public:
 
         ~File() = default;
-
-        File(const std::string& fileName)
-            : fileName(fileName) {}
-
-    private:
-
-        bool prepareFile(DWORD flag)
-        {
-            this->fileHandle = CreateFileA(this->fileName.c_str(),
-                GENERIC_READ | GENERIC_WRITE, 0, nullptr,
-                flag, FILE_ATTRIBUTE_NORMAL, nullptr);
-
-            if (this->fileHandle == INVALID_HANDLE_VALUE) //Validate.
-            {
-                return false; //Return false since handle is invalid.
-            }
-
-            return true; //Return true since all went as it expected.
-        }
+        File(const std::string& fileName) : fileName(fileName) {}
 
     public:
 
         bool Read(std::string& content)
         {
-            if (!this->prepareFile(OPEN_ALWAYS)) //Prepare the file.
+            auto fileHandle = HANDLE(); //Define the file handle obj.
+
+            if (!this->GetFileHandle(OPEN_ALWAYS, fileHandle))
             {
                 return false; //Return false since handle is invalid.
             }
 
             const auto fileSize = GetFileSize( //Retrieve file size.
-                this->fileHandle, nullptr);
+                fileHandle, nullptr);
 
             auto buffer = new char[fileSize]; //Setup the buffer ptr.
 
             auto bytesRead = DWORD(); //Define readed bytes size obj.
 
-            const auto result = ReadFile(this->fileHandle, buffer,
+            const auto result = ReadFile(fileHandle, buffer,
                 fileSize, &bytesRead, nullptr);
+
+            CloseHandle(fileHandle); //Close the created file handle.
 
             if (result != 0) //Check if the readed results were valid.
             {
@@ -70,46 +51,60 @@ namespace win32
 
         bool Write(const std::string& content)
         {
-            if (!this->prepareFile(CREATE_ALWAYS)) //Prepare the file.
+            auto fileHandle = HANDLE(); //Define the file handle obj.
+
+            if (!this->GetFileHandle(CREATE_ALWAYS, fileHandle))
             {
                 return false; //Return false since handle is invalid.
             }
 
             auto bytesWritten = DWORD(); //Define written bytes object.
 
-            const auto result = WriteFile(this->fileHandle,
+            const auto result = WriteFile(fileHandle,
                 content.c_str(), content.size(), &bytesWritten, NULL);
 
+            CloseHandle(fileHandle); //Close the created file handle.
 
             return result != 0; //Check if the file results are valid.
         }
 
+    private:
 
-        void Close()
+        bool GetFileHandle(DWORD flag, HANDLE& handle)
         {
-            CloseHandle(this->fileHandle); //Close created file handle.
+            handle = CreateFileA(this->fileName.c_str(), 0xc0000000,
+                0, nullptr, flag, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+            if (handle == INVALID_HANDLE_VALUE) //Validate the handle.
+            {
+                return false; //Return false the handle is not valid.
+            }
+
+            return true; //Return true since all went as we expected.
         }
     };
 
+
     inline bool DirectoryExists(const std::string& path)
     {
-        DWORD attribs = ::GetFileAttributesA(path.data());
-        if (attribs == INVALID_FILE_ATTRIBUTES)
+        const auto attributes = GetFileAttributesA(path.data()); //Get.
+
+        if (attributes == INVALID_FILE_ATTRIBUTES) //Validate file att.
         {
-            return false;
+            return false; //Return false since invalid file attributes.
         }
 
-        return (attribs & FILE_ATTRIBUTE_DIRECTORY);
+        return (attributes & FILE_ATTRIBUTE_DIRECTORY); //Check direct.
     }
 
-    inline bool CreateNewDirectory(const std::string& path, bool createAlways = false)
+    inline bool CreateNewDirectory(
+        const std::string& path, bool createAlways = false)
     {
-        if (DirectoryExists(path.data()) && createAlways == false)
+        if (DirectoryExists(path.data()) && createAlways == 0) //Check.
         {
-            return false;
+            return false; //Return false since the directory is active.
         }
 
-        return CreateDirectoryA(path.c_str(), nullptr) != 0; //Do.
+        return CreateDirectoryA(path.c_str(), nullptr) != 0; //Make it.
     }
-
 }
